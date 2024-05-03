8c88a80020c36e1b223c8e558175012bb4576ba6
tripathianurag63 <anuragtripathi209306@gmail.com>
2024-05-03 21:24:32 +0530

    my node js repo

---
+++ hello.js
@@ -0,0 +1,6 @@
+const {add, sub} = require("./math");
+
+const math = require("crypto");
+// nodejs me bahot sare node modules availabale h 
+//jinka use hm apne progrsm me kar skte h
+console.log(math(2,4));
