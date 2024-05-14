77567e8b7a720671827579003e09f10c9cd53958
tripathianurag63 <anuragtripathi209306@gmail.com>
2024-05-14 12:39:11 +0530

    Modules

--- hello.js
+++ hello.js
@@ -1,5 +1,5 @@
 const {add, sub} = require("./math");
-
+// this is a way to import module like we did in react
 const math = require("crypto");
 // nodejs me bahot sare node modules availabale h 
 //jinka use hm apne progrsm me kar skte h
--- math.js
+++ math.js
@@ -1,12 +1,16 @@
-function add(a,b){
-    return a+b;
-}
+//function add(a,b){
+  //  return a+b;
+//}
 
-function sub(a,b){
-    return a-b;
-}
+//function sub(a,b){
+  //  return a-b;
+//}
 
-module.exports = {
-    add,
-    sub,
-};
+//module.exports = {
+//    add,
+//    sub,
+//};
+
+// this is also a way to export it
+exports.add = (a,b) => a+b;
+exports.add = (a,b) => a-b;
